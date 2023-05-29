612b841bffbb6ec162fb529030c1608cb5f4c3b4
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-29 11:08:21 -0400

    Route of new product

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
+import NewProduct from "./pages/newProduct";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}></Route>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/product/new" element={<NewProduct />}></Route>
+                </Route>
             </Routes>
         </BrowserRouter>
     );
