8bd924221d5a9e608cff8647d2846823c018f103
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-29 10:52:42 -0400

    Use Link of react router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/" className="flex items-center">
+                <Link to="/" className="flex items-center">
                     <img src="favicon.svg" className="h-8 mr-3" alt="Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                         Redux App
                     </span>
-                </a>
+                </Link>
                 <div className="flex md:order-2">
-                    <button
-                        type="button"
+                    <Link
+                        to="/newProject"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition duration-150"
                     >
                         Agregar Producto
-                    </button>
+                    </Link>
                     <button
                         data-collapse-toggle="navbar-cta"
                         type="button"
