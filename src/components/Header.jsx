a65083fafe5238c62ff22e9de6e8b22aa5f33327
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-29 11:07:20 -0400

    New route in button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
                 </Link>
                 <div className="flex md:order-2">
                     <Link
-                        to="/newProject"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition duration-150"
+                        to="/product/new"
+                        className="text-white bg-blue-700 hover:bg-blue-800 outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700  transition duration-150"
                     >
                         Agregar Producto
                     </Link>
