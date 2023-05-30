a1cd32de5b5770a1c3d4c83b10f146230c0d6df3
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-30 16:28:02 -0400

    Use toastify

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
 	return (
 		<>
+			<ToastContainer theme='dark' />
 			<Header />
 			<main className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 mt-5'>
 				<Outlet />
