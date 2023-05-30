4c097b28ff8a920352699d5b33aa1762fb1183b3
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-30 14:09:15 -0400

    Edit producto by id

---
+++ src/pages/editProductId.jsx
@@ -0,0 +1,12 @@
+import FormProduct from '../components/FormProduct';
+
+const EditProductId = () => {
+	return (
+		<div className='w-full'>
+			<h1 className='text-white text-2xl font-bold mb-8'>Editar producto</h1>
+			<FormProduct />
+		</div>
+	);
+};
+
+export default EditProductId;
