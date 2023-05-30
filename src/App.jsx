aceaf950b7179dee30333868903ddb05ad9477ff
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-30 14:08:46 -0400

    Route edit product

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import NewProduct from './pages/newProduct';
-import EditProduct from './pages/editProduct';
+import Index from './pages';
+import EditProductId from './pages/editProductId';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<Layout />}>
+					<Route index element={<Index />} />
 					<Route path='/product/new' element={<NewProduct />}></Route>
-					<Route path='/product/edit' element={<EditProduct />} />
+					<Route path='/product/edit/:id' element={<EditProductId />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
