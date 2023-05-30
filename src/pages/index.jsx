314849e96a14c6e72a21a5c28cac089ccaf1759a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-30 14:09:32 -0400

    Table of products

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import TableProduct from '../components/TableProduct';
+
+const Index = () => {
+	return (
+		<div className='w-full'>
+			<h1 className='text-white text-2xl font-bold mb-8'>Productos</h1>
+			<TableProduct />
+		</div>
+	);
+};
+
+export default Index;
