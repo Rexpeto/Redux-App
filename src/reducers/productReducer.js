44e82a964caebeb92de9d0163a82b5190ebeddcc
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-31 10:06:34 -0400

    Use types and logic

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,4 +1,4 @@
-import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS } from '../types';
 
 const initialState = {
 	products: [],
@@ -7,6 +7,17 @@ const initialState = {
 
 export default (state = initialState, action) => {
 	switch (action.type) {
+		case ADD_PRODUCT:
+			return {
+				...state,
+				loading: action.payload,
+			};
+		case ADD_PRODUCT_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				products: [...state.products, action.payload],
+			};
 		default:
 			return state;
 	}
