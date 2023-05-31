5407c5aea74aba7f41722f44b652c679e55625af
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-31 10:30:05 -0400

    Use Spinner with loading state

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import Spinner from '../components/Spinner';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from 'react-redux';
 
 const Layout = () => {
+	const loading = useSelector(state => state.products.loading);
+
 	return (
 		<>
 			<ToastContainer theme='dark' />
 			<Header />
 			<main className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 mt-5'>
-				<Outlet />
+				{loading ? <Spinner /> : <Outlet />}
 			</main>
 		</>
 	);
