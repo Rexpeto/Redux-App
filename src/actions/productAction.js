be1b450ff13070518e23ee2b0d2c9b4b93addcbe
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-31 10:05:17 -0400

    Creation product

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -1,8 +1,30 @@
-import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
+import { toast } from 'react-toastify';
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS } from '../types';
+import clientAxios from '../config/axios';
 
 /**
  * Create new producto
  */
 export const createProductAction = product => {
-	return () => console.log(product);
+	return async dispatch => {
+		dispatch(addProduct());
+
+		try {
+			await clientAxios.post('/products', product);
+			dispatch(addProductSuccess(product));
+			toast.success('Producto agreagado con exito');
+		} catch (e) {
+			toast.error('Oops!! Ocurrió un error');
+		}
+	};
 };
+
+const addProduct = () => ({
+	type: ADD_PRODUCT,
+	payload: true,
+});
+
+const addProductSuccess = product => ({
+	type: ADD_PRODUCT_SUCCESS,
+	payload: product,
+});
