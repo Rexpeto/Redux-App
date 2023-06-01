5683c51232a8a12a5998d78a29d79f56d755c6ef
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-01 17:37:16 -0400

    Use form edit product

--- src/pages/editProductId.jsx
+++ src/pages/editProductId.jsx
@@ -1,10 +1,22 @@
-import FormProduct from '../components/FormProduct';
+import { useNavigate } from 'react-router-dom';
+import FormProductEdit from '../components/FormProductEdit';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const EditProductId = () => {
+	const product = useSelector(state => state.products.product);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!product.id) {
+			navigate('/');
+		}
+	}, []);
+
 	return (
 		<div className='w-full'>
 			<h1 className='text-white text-2xl font-bold mb-8'>Editar producto</h1>
-			<FormProduct />
+			<FormProductEdit />
 		</div>
 	);
 };
